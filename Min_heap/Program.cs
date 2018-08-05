82c944c57022ff15eea1801cb66d4c69f8bb9717
Sevak775 <levon.lev.12@mail.ru>
2018-08-05 21:14:19 +0400

    adding Min_Heap

---
+++ Min_heap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min_heap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinHeap<int> h = new MinHeap<int>();
+            h.Insert(5);
+            h.Insert(12);
+            h.Insert(3);
+            h.Insert(7);
+            h.Insert(1);
+            h.ExtractMin();
+            Console.ReadLine();
+        }
+    }
+}
